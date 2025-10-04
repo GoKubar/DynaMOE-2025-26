659c92c28e173dd3545ee41b3cbc9da049735f3f
Kai Fu <kaifu7k@gmail.com>
2025-10-04 11:13:26 -0400

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SimpleFourMotorAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SimpleFourMotorAuto.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.opmodes.auto;
+package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "3 Motor Test")
-public class SimpleThreeMotorAuto extends LinearOpMode {
+@Autonomous(name = "4 Motor Test")
+public class SimpleFourMotorAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() {
@@ -14,9 +14,11 @@ public class SimpleThreeMotorAuto extends LinearOpMode {
         DcMotor leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
         DcMotor leftBack   = hardwareMap.get(DcMotor.class, "leftBack");
         DcMotor rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
 
         // reverse to go foward
         rightBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
         // already works
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
@@ -28,7 +30,7 @@ public class SimpleThreeMotorAuto extends LinearOpMode {
 
         double rightPower = 0.2;
         //missing the wheel so cut inhalf
-        double leftPower = rightPower / 2.0;
+        double leftPower = 0.2;
 
         // driivng
         telemetry.addData("status", "driving");
@@ -39,15 +41,16 @@ public class SimpleThreeMotorAuto extends LinearOpMode {
         leftFront.setPower(leftPower);
         leftBack.setPower(leftPower);
         rightBack.setPower(rightPower);
+        rightFront.setPower(rightPower);
 
-        // driving for 2 secs
-        sleep(2000);
+        // driving for 4 secs
+        sleep(4000);
 
         // stop
         leftFront.setPower(0);
         leftBack.setPower(0);
         rightBack.setPower(0);
-
+        rightFront.setPower(0);
         telemetry.addData("status", "complete");
         telemetry.update();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldCentricMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldCentricMecanumDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.auto;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
