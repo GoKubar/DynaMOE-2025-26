df1c29ef000c01a4d4c1e27b774da12887656fd9
Chetan2871 <chetan.kasukurthi@gmail.com>
2025-10-03 20:15:49 -0400

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RobotCentricMecanumDrive.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class RobotCentricMecanumDrive extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor leftBack   = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        //reverse later
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double leftFrontPower = (y + x + rx) / denominator;
+            double leftBackPower = (y - x + rx) / denominator;
+            double rightFrontPower = (y - x - rx) / denominator;
+            double rightBackPower = (y + x - rx) / denominator;
+
+            leftFront.setPower(leftFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightBack.setPower(rightBackPower);
+            rightFront.setPower(rightFrontPower);
+        }
+    }
+}
